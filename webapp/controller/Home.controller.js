2750830c136f0f9aee3a994c64add8b043797945
Marcattack <marcuhart@gmail.com>
2016-05-24 11:27:38 +0200

    Step 3 - Catch Invalid Hashes

--- webapp/controller/Home.controller.js
+++ webapp/controller/Home.controller.js
@@ -3,7 +3,7 @@ sap.ui.define([
 ], function (Controller) {
     "use strict";
     
-    return Controller.extend("sap.ui.demo.va.controller.Home", {
+    return Controller.extend("sap.ui.demo.nav.controller.Home", {
         
     });
 });
