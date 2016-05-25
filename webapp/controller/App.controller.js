29c033bfec28a7a7bfe19a5795fae12c7bf8badd
Marcattack <marcuhart@gmail.com>
2016-05-25 16:50:19 +0200

    Step 17 - Listen to Matched Events of Any Route

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -6,12 +6,18 @@ sap.ui.define([
 	return BaseController.extend("sap.ui.demo.nav.controller.App", {
 		onInit: function () {
 			var oRouter = this.getRouter(); // récupére la route de BaseController
-			oRouter.AttachBypassed(function (oEvent) {
+			oRouter.attachBypassed(function (oEvent) {
 				var sHash = oEvent.getParameter("hash");
 				// do something here, i.e. send logging data to the backend for analysis
 				// telling what resource the user tried to access...
-				jQuery.sap.log.info("Sorry, but the hash '" + sHash + "'is invalid.", "The resource was not found.");
+				jQuery.sap.log.info("Sorry, but the hash '" + sHash + "' is invalid.", "The resource was not found.");
 			});
+			oRouter.attachRouteMatched(function (oEvent){
+				var sRouteName = oEvent.getParameter("name");
+				// do something, i.e. send usage statistics to backend
+				// in order to improve our app and the user experience (Build-Measure-Learn cycle)
+				jQuery.sap.log.info("User accessed route " + sRouteName + ", timestamp = " + new Date().getTime());
+			})
 		}
 	});
 });
